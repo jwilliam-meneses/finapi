82245187214bb378527bbe3acc8cfd1c329cc9cb
José William <jwilliam.anjos@gmail.com>
2022-04-28 14:50:37 -0300

    Add delete account feature

--- src/index.js
+++ src/index.js
@@ -69,6 +69,14 @@ app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send()
 })
 
+app.delete('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+
+  customers.splice(customer, 1)
+
+  return response.status(200).json(customers)
+})
+
 app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request
 
